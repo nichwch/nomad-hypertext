7a064d47eaa2dc86d67720344cb589242e27523a
nichwch <nichwch@gmail.com>
2023-10-20 21:42:35 -0400

    test

--- src/Overlay.svelte
+++ src/Overlay.svelte
@@ -1,15 +1,26 @@
 <script>
   /** @type {string[]} */
   export let segments = [];
+  const searchSegment = async (/** @type {string} */ segment) => {
+    const searchResults =
+      //@ts-ignore
+      (await window.electronAPI.vectorQuery(searchQuery))?.hits || [];
+    console.log(searchResults);
+  };
 </script>
 
 {#each segments as segment}
   {#if segment.length > 0}
     <div class="relative block">
       {segment}
-      <div class="absolute opacity-50 top-0 left-full pl-3 hover:text-red-800">
+      <button
+        on:click={() => {
+          searchSegment(segment);
+        }}
+        class="absolute opacity-50 top-0 left-full pl-3 hover:text-red-800"
+      >
         test
-      </div>
+      </button>
     </div>
   {:else}
     <br />
