d8666ac7a9351df5280819b3886ddbbe01d8c775
nichwch <nichwch@gmail.com>
2023-12-04 13:26:15 -0500

    factor search filters into separate component

---
+++ src/SearchFilters.svelte
@@ -0,0 +1,58 @@
+<script>
+  /**
+   * @type {() => void}
+   */
+  export let refreshResults;
+  export let threshold = 70;
+  /**
+   * @type {(arg0: number) => void}
+   */
+  export let setThreshold;
+  $: {
+    setThreshold(threshold);
+    refreshResults();
+  }
+</script>
+
+<div
+  class="bg-orange-300 bg-opacity-30 border-b border-b-black px-2 pb-1 bg-texture-orange"
+>
+  <label class="block py-1">
+    <span class="inline-block">similarity threshold: </span>
+    <input
+      class="bg-orange-200 border border-orange-900 pl-1 text-red-800 mr-0 inline-block"
+      type="number"
+      bind:value={threshold}
+      min={0}
+      max={95}
+    /><span
+      class="text-red-800 px-1 mx-0 bg-orange-200 border border-orange-900 border-l-0 inline-block"
+    >
+      %
+    </span>
+  </label>
+  <div>
+    <button
+      class="small-button bg-orange-400 hover:bg-orange-500"
+      on:click={() => {
+        threshold = 80;
+      }}
+      >80% (medium)
+    </button>
+    <button
+      class="small-button bg-yellow-400 hover:bg-yellow-500"
+      on:click={() => {
+        threshold = 85;
+      }}
+      >85% (strong)
+    </button>
+    <button
+      class="small-button bg-green-400 hover:bg-green-500"
+      on:click={() => {
+        threshold = 95;
+      }}
+      >90% (very strong)
+    </button>
+  </div>
+  <div />
+</div>
--- src/routes/[...noteName]/+page.svelte
+++ src/routes/[...noteName]/+page.svelte
@@ -4,6 +4,7 @@
   import Overlay from "../../Overlay.svelte";
   import { page } from "$app/stores";
   import { afterNavigate } from "$app/navigation";
+  import SearchFilters from "../../SearchFilters.svelte";
   // @ts-ignore
   let notesDir = window.electronAPI.getNoteDir();
   /** @type {string|null}*/
@@ -202,52 +203,11 @@ we copy it into a separate variable
           </div>
         </div>
         {#if showingFilters}
-          <div
-            class="bg-orange-300 bg-opacity-30 border-b border-b-black px-2 pb-1 bg-texture-orange"
-          >
-            <label class="block py-1">
-              <span class="inline-block">similarity threshold: </span>
-              <input
-                class="bg-orange-200 border border-orange-900 pl-1 text-red-800 mr-0 inline-block"
-                type="number"
-                bind:value={threshold}
-                on:change={refreshResults}
-                min={0}
-                max={95}
-              /><span
-                class="text-red-800 px-1 mx-0 bg-orange-200 border border-orange-900 border-l-0 inline-block"
-              >
-                %
-              </span>
-            </label>
-            <div>
-              <button
-                class="small-button bg-orange-400 hover:bg-orange-500"
-                on:click={() => {
-                  threshold = 80;
-                  refreshResults();
-                }}
-                >80% (medium)
-              </button>
-              <button
-                class="small-button bg-yellow-400 hover:bg-yellow-500"
-                on:click={() => {
-                  threshold = 85;
-                  refreshResults();
-                }}
-                >85% (strong)
-              </button>
-              <button
-                class="small-button bg-green-400 hover:bg-green-500"
-                on:click={() => {
-                  threshold = 95;
-                  refreshResults();
-                }}
-                >90% (very strong)
-              </button>
-            </div>
-            <div />
-          </div>
+          <SearchFilters
+            {refreshResults}
+            {threshold}
+            setThreshold={(n) => (threshold = n)}
+          />
         {/if}
       </div>
       <div class="overflow-y-auto p-2">
